73364c32752a8c989096a10730e5f76f6a1582ee
Anas LRHAZOULI <anaslrh2@outlook.com>
2024-10-10 16:19:23 +0200

    test

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/detailsPage` | `/(tabs)/menuPage` | `/(tabs)/review` | `/_sitemap` | `/detailsPage` | `/menuPage` | `/review`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/AvisPage` | `/(tabs)/details` | `/(tabs)/detailsPage` | `/(tabs)/menuPage` | `/AvisPage` | `/_sitemap` | `/details` | `/detailsPage` | `/menuPage`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
